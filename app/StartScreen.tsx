5ded7f3bee388221f466cbb9c7b081da9d1e5dc4
Thilochan Pathmanathan <thilochan.tech@gmail.com>
2025-10-28 11:16:24 -0400

    feat: add MainButton component, theme setup, and font initialization
    - Implemented MainButton component for reusable button functionality with customizable title, background color, and text color.
    
    - Added theme file defining color and font constants for consistent styling across the application.
    
    - Updated StartScreen with background image, app title, and MainButton using the theme.
    
    - Configured global font loading in _layout.tsx for app-wide access to Poppins font.

--- app/StartScreen.tsx
+++ app/StartScreen.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text } from "react-native";
+import { COLORS, FONTS } from "../theme";
+
+import MainButton from "@/components/MainButton";
+
+const backgroundImage = require("../assets/images/start-screen-background.jpg");
 
 export default function StartScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Start Screen</Text>
-    </View>
+    <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+      <Text style={styles.appTitle}>NiteMed</Text>
+      <MainButton title="Start" onPress={() => alert("Get Started pressed!")} />
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  appTitle: {
+    flex: 1,
+    marginTop: 50,
+    fontSize: 64,
+    fontFamily: FONTS.poppins,
+    color: COLORS.white,
+    textShadowColor: COLORS.black,
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 1,
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
+import { Poppins_700Bold, useFonts } from "@expo-google-fonts/poppins";
 import { Stack } from "expo-router";
+import { Text } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="StartScreen" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
