1a83e1c832bec2e11a93c42344b21b2fbe3b3287
Thilochan Pathmanathan <thilochan.tech@gmail.com>
2025-11-03 09:16:57 -0500

    feat: add RegistrationScreen component with basic layout

---
+++ app/RegistrationScreen.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function RegistrationScreen() {
+  return (
+    <View>
+      <Text>Registration</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
