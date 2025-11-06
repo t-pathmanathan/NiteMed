5a5b6d0f1d6119ba51dcbc872ee2096c60a3133e
Thilochan Pathmanathan <thilochan.tech@gmail.com>
2025-11-06 09:44:27 -0500

    feat: add LoginScreen component with basic layout

---
+++ app/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
